1c5753f33e9a4c8f8a6bc4f9a09ab313f1dfd6d2
Jay Lee <jaylee@reed.edu>
2018-02-26 22:47:18 -0800

    2017 update

--- data-raw/airports.R
+++ data-raw/airports.R
@@ -10,7 +10,7 @@ if (!file.exists("data-raw/airports.dat")) {
 }
 
 raw <- read_csv("data-raw/airports.dat",
-  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst", "tzone")
+  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst", "tzone", "type", "source")
 )
 
 airports <- raw %>%
