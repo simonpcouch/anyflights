2ae5a9d4c6bd70585297b958464828eef766f2b1
Simon Patrick Couch <couchs@rstudio-dev.reed.edu>
2018-02-27 20:51:21 -0800

    now grabs the previous year, fixed row binding

--- data-raw/weather.R
+++ data-raw/weather.R
@@ -7,14 +7,16 @@ library(readr)
 
 # Download ---------------------------------------------------------------------
 
+last_year <- as.numeric(substr(Sys.time(), 1, 4)) - 1
+
 get_asos <- function(station) {
   url <- "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
   query <- list(
     station = station, data = "all",
-    year1 = "2013", month1 = "1", day1 = "1",
-    year2 = "2013", month2 = "12", day2 = "31", tz = "GMT",
+    year1 = as.character(last_year), month1 = "1", day1 = "1",
+    year2 = as.character(last_year), month2 = "12", day2 = "31", tz = "GMT",
     format = "comma", latlon = "no", direct = "yes")
-
+  
   dir.create("data-raw/weather", showWarnings = FALSE, recursive = TRUE)
   r <- GET(url, query = query, write_disk(paste0("./data-raw/weather", station, ".csv")))
   stop_for_status(r)
@@ -28,25 +30,25 @@ lapply(missing, get_asos)
 # Load ------------------------------------------------------------------------
 
 paths <- dir("data-raw/weather", full.names = TRUE)
-all <- lapply(paths, read_csv, skip = 4, na = "M", col_names = FALSE, col_types =
-  cols(
-    .default = col_double(),
-    X1 = col_character(),
-    X2 = col_datetime(format = ""),
-    X12 = col_character(),
-    X13 = col_character(),
-    X14 = col_character(),
-    X15 = col_character(),
-    X16 = col_character(),
-    X21 = col_character(),
-    X22 = col_character()
-  )
+col_types <- cols(
+  .default = col_double(),
+  station = col_character(),
+  valid = col_datetime(format = ""),
+  skyc1 = col_character(),
+  skyc2 = col_character(),
+  skyc3 = col_character(),
+  skyc4 = col_character(),
+  wxcodes = col_character(),
+  metar = col_character()
 )
+
+all <- lapply(paths, read_csv, comment = "#", na = "M",
+              col_names = TRUE, col_types = col_types)
 
 raw <- bind_rows(all)
 names(raw) <- c("station", "time", "tmpf", "dwpf", "relh", "drct", "sknt",
-  "p01i", "alti", "mslp", "vsby", "gust", "skyc1", "skyc2", "skyc3", "skyc4",
-  "skyl1", "skyl2", "skyl3", "skyl4", "metar")
+                "p01i", "alti", "mslp", "vsby", "gust", "skyc1", "skyc2", "skyc3", "skyc4",
+                "skyl1", "skyl2", "skyl3", "skyl4", "metar")
 
 weather <- raw %>%
   select(
@@ -59,7 +61,7 @@ weather <- raw %>%
     wind_speed = as.numeric(wind_speed) * 1.15078, # convert to mpg
     wind_gust = as.numeric(wind_speed) * 1.15078
   ) %>%
-  mutate(year = 2013, month = month(time), day = mday(time), hour = hour(time)) %>%
+  mutate(year = last_year, month = month(time), day = mday(time), hour = hour(time)) %>%
   group_by(station, month, day, hour) %>%
   filter(row_number() == 1) %>%
   select(origin = station, year:hour, temp:visib) %>%
