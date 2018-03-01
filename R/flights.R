d57e9d90c9b79745a2c5746e7e30b00238248d3b
Jay Lee <jaylee@reed.edu>
2018-03-01 14:33:11 -0800

    2017 update

--- R/flights.R
+++ R/flights.R
@@ -1,7 +1,7 @@
 #' Flights data
 #'
 #' On-time data for all flights that departed NYC (i.e. JFK, LGA or EWR) in
-#' 2013.
+#' 2017.
 #'
 #' @source RITA, Bureau of transportation statistics,
 #'  \url{http://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236}
