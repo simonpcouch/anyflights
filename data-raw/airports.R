43c1fdfd034c056ba8e566107eb7fa5fbd506190
Jay Lee <jaylee@reed.edu>
2018-03-05 01:13:59 -0800

    Fix .dat read issues

--- data-raw/airports.R
+++ data-raw/airports.R
@@ -9,9 +9,11 @@ if (!file.exists("data-raw/airports.dat")) {
   )
 }
 
-raw <- read_csv("data-raw/airports.dat",
-  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst", "tzone", "type", "source")
+raw <- read_delim("data-raw/airports.dat", ",",
+  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst", "tzone", "type", "source"),
+  escape_double = FALSE, na = c("", "NA", "\\N")
 )
+
 
 airports <- raw %>%
   filter(country == "United States", faa != "") %>%
@@ -27,7 +29,7 @@ airports %>%
   geom_point(aes(colour = factor(tzone)), show.legend = FALSE) +
   coord_quickmap() +
   theme_void()
-ggsave("data-raw/airports.svg", width = 8, height = 6)
+try(ggsave("data-raw/airports.svg", width = 8, height = 6), silent = TRUE)
 
 write_csv(airports, "data-raw/airports.csv")
 save(airports, file = "data/airports.rda")
