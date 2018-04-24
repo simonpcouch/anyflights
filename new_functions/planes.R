efb33d0adb76153f8129acecdce027666afa0d9e
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-04-24 11:35:39 -0700

    add year argument

---
+++ new_functions/planes.R
@@ -0,0 +1,63 @@
+get_planes <- function(year) {
+ 
+# Update URL from
+# http://www.faa.gov/licenses_certificates/aircraft_certification/aircraft_registry/releasable_aircraft_download/
+src <- paste0("http://registry.faa.gov/database/yearly/ReleasableAircraft.", year, ".zip")
+lcl <- "data-raw/planes"
+
+tmp <- tempfile(fileext = ".zip")
+download.file(src, tmp) 
+
+  dir.create(lcl)
+  unzip(tmp, exdir = lcl, junkpaths = TRUE)
+}
+
+master <- read.csv("data-raw/planes/MASTER.txt", stringsAsFactors = FALSE, strip.white = TRUE)
+names(master) <- tolower(names(master))
+
+keep <- master %>%
+  tbl_df() %>%
+  select(nnum = n.number, code = mfr.mdl.code, year = year.mfr)
+
+ref <- read.csv("data-raw/planes//ACFTREF.txt", stringsAsFactors = FALSE,
+  strip.white = TRUE)
+names(ref) <- tolower(names(ref))
+
+ref <- ref %>%
+  tbl_df() %>%
+  select(code, mfr, model, type.acft, type.eng, no.eng, no.seats, speed)
+
+# Combine together
+
+all <- keep %>%
+  inner_join(ref) %>%
+  select(-code)
+all$speed[all$speed == 0] <- NA
+all$no.eng[all$no.eng == 0] <- NA
+all$no.seats[all$no.seats == 0] <- NA
+
+engine <- c("None", "Reciprocating", "Turbo-prop", "Turbo-shaft", "Turbo-jet",
+  "Turbo-fan", "Ramjet", "2 Cycle", "4 Cycle", "Unknown", "Electric", "Rotary")
+all$engine <- engine[all$type.eng + 1]
+all$type.eng <- NULL
+
+acft <- c("Glider", "Balloon", "Blimp/Dirigible", "Fixed wing single engine",
+  "Fixed wing multi engine", "Rotorcraft", "Weight-shift-control",
+  "Powered Parachute", "Gyroplane")
+all$type <- acft[all$type.acft]
+all$type.acft <- NULL
+
+all$tailnum <- paste0("N", all$nnum)
+
+load("data/flights.rda")
+
+planes <- all %>%
+  select(
+    tailnum, year, type, manufacturer = mfr, model = model,
+    engines = no.eng, seats = no.seats, speed, engine
+  ) %>%
+  semi_join(flights, "tailnum") %>%
+  arrange(tailnum)
+
+save(planes, file = "data/planes.rda", compress = "xz")
+}
