27630e3e7065f9b40abc80e3f0bb5d98f7bdc511
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-05-06 21:10:02 -0700

    add documentation

--- R/make_flights.R
+++ R/make_flights.R
@@ -1,3 +1,12 @@
+#' Generate a folder of flights datasets for a given year and airport
+#' 
+#' @param station The airport of interest (use the airport code)
+#' @param year The year of interest, as an integer
+#' @return A folder named with "airport code" "flights" "year" convention 
+#' (i.e. pdxflights15) containing datasets about flights & relevant metadata
+#' @examples
+#' make_flights(station = "PDX", year = 2015)
+#' @export
 make_flights <- function(station, year) {
   
   # Create Subdirectory ----------------------
