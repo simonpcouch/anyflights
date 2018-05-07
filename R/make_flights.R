4162fe7377e72dfa6e278dee322eeba4a8630a6a
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-05-06 20:38:39 -0700

    make namespace explicit

--- R/make_flights.R
+++ R/make_flights.R
@@ -18,15 +18,15 @@ make_flights <- function(station, year) {
   
     download_month <- function(year = year, month) {
       fl_url <- flight_url(year, month)
-      if (url.exists(fl_url)) {
+      if (RCurl::url.exists(fl_url)) {
         flight_temp <- tempfile(fileext = ".zip")
-        download.file(fl_url, flight_temp)
+        utils::download.file(fl_url, flight_temp)
       } else stop(sprintf("Can't access flight data for supplied year. Check date of 'Latest Available Data' for 'Airline On-Time Performance Data' on \n https://www.transtats.bts.gov/releaseinfo.asp"))
       
-      flight_files <- unzip(flight_temp, list = TRUE)
+      flight_files <- utils::unzip(flight_temp, list = TRUE)
       # Only extract biggest file
       flight_csv <- flight_files$Name[order(flight_files$Length, decreasing = TRUE)[1]]
-      unzip(flight_temp, exdir = flight_exdir, junkpaths = TRUE, files = flight_csv)
+      utils::unzip(flight_temp, exdir = flight_exdir, junkpaths = TRUE, files = flight_csv)
       
       flight_src <- paste0(subdir, "/flights/", flight_csv)
       flight_dst <- paste0(subdir, "/flights/", year, "-", month, ".csv")
@@ -37,17 +37,17 @@ make_flights <- function(station, year) {
     lapply(months, download_month, year = year)
     
     get_flight_data <- function(path) {
-      col_types <- cols(
-        DepTime = col_integer(),
-        ArrTime = col_integer(),
-        CRSDepTime = col_integer(),
-        CRSArrTime = col_integer(),
-        Carrier = col_character(),
-        UniqueCarrier = col_character()
+      col_types <- readr::cols(
+        DepTime = readr::col_integer(),
+        ArrTime = readr::col_integer(),
+        CRSDepTime = readr::col_integer(),
+        CRSArrTime = readr::col_integer(),
+        Carrier = readr::col_character(),
+        UniqueCarrier = readr::col_character()
       )
       
-      suppressWarnings(read_csv(path, col_types = col_types)) %>%
-        select(
+      suppressWarnings(readr::read_csv(path, col_types = col_types)) %>%
+        dplyr::select(
           year = Year, month = Month, day = DayofMonth,
           dep_time = DepTime, sched_dep_time = CRSDepTime, dep_delay = DepDelay,
           arr_time = ArrTime, sched_arr_time = CRSArrTime, arr_delay = ArrDelay,
@@ -55,13 +55,13 @@ make_flights <- function(station, year) {
           origin = Origin, dest = Dest,
           air_time = AirTime, distance = Distance
         ) %>%
-        filter(origin %in% station) %>%
-        mutate(
+        dplyr::filter(origin %in% station) %>%
+        dplyr::mutate(
           hour = sched_dep_time %/% 100,
           minute = sched_dep_time %% 100,
           time_hour = lubridate::make_datetime(year, month, day, hour, 0, 0)
         ) %>%
-        arrange(year, month, day, dep_time)
+        dplyr::arrange(year, month, day, dep_time)
     }
     
     all <- lapply(dir(flight_exdir, full.names = TRUE), get_flight_data)
@@ -74,7 +74,7 @@ make_flights <- function(station, year) {
   # Download Weather Data --------------------
     
     weather_url <- "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
-    if(!(url.exists(weather_url))) stop("Can't access `weather` querying link")
+    if(!(RCurl::url.exists(weather_url))) stop("Can't access `weather` querying link")
     weather_query <- list(
       station = station, data = "all",
       year1 = as.character(year), month1 = "1", day1 = "1",
@@ -84,20 +84,20 @@ make_flights <- function(station, year) {
     weather_subdir <- paste(subdir, "/weather")
     dir.create(weather_subdir, showWarnings = FALSE, recursive = FALSE)
     
-    r <- GET(weather_url, query = weather_query, write_disk(paste0("./", weather_subdir, "/", station, ".csv"), overwrite = TRUE))
-    stop_for_status(r, "Can't access `weather` link for requested location and date range. \n Check data availability at `https://mesonet.agron.iastate.edu`")
+    r <- httr::GET(weather_url, query = weather_query, write_disk(paste0("./", weather_subdir, "/", station, ".csv"), overwrite = TRUE))
+    httr::stop_for_status(r, "Can't access `weather` link for requested location and date range. \n Check data availability at `https://mesonet.agron.iastate.edu`")
     
     weather_paths <- dir(weather_subdir, full.names = TRUE)
-    weather_col_types <- cols(
-      .default = col_double(),
-      station = col_character(),
+    weather_col_types <- readr::cols(
+      .default = readr::col_double(),
+      station = readr::col_character(),
       valid = col_datetime(format = ""),
-      skyc1 = col_character(),
-      skyc2 = col_character(),
-      skyc3 = col_character(),
-      skyc4 = col_character(),
-      wxcodes = col_character(),
-      metar = col_character()
+      skyc1 = readr::col_character(),
+      skyc2 = readr::col_character(),
+      skyc3 = readr::col_character(),
+      skyc4 = readr::col_character(),
+      wxcodes = readr::col_character(),
+      metar = readr::col_character()
     )
     
     weather_all <- lapply(weather_paths, read_csv, comment = "#", na = "M",
@@ -110,13 +110,13 @@ make_flights <- function(station, year) {
     
     weather <-
       weather_raw %>%
-      rename_(time = ~valid) %>%
-      select_(
+      dplyr::rename(time = ~valid) %>%
+      dplyr::select(
         ~origin, ~time, temp = ~tmpf, dewp = ~dwpf, humid = ~relh,
         wind_dir = ~drct, wind_speed = ~sknt, wind_gust = ~gust,
         precip = ~p01i, pressure = ~mslp, visib = ~vsby
       ) %>%
-      mutate_(
+      dplyr::mutate(
         time = ~as.POSIXct(strptime(time, "%Y-%m-%d %H:%M")),
         wind_speed = ~as.numeric(wind_speed) * 1.15078, # convert to mpg
         wind_gust = ~as.numeric(wind_speed) * 1.15078,
@@ -124,12 +124,12 @@ make_flights <- function(station, year) {
         month = ~lubridate::month(time),
         day = ~lubridate::mday(time),
         hour = ~lubridate::hour(time)) %>%
-      group_by_(~origin, ~month, ~day, ~hour) %>%
-      filter_(~ row_number() == 1) %>%
-      select_(~origin, ~year:hour, ~temp:visib) %>%
-      ungroup() %>%
-      filter_(~!is.na(month)) %>%
-      mutate_(
+      dplyr::group_by(~origin, ~month, ~day, ~hour) %>%
+      dplyr::filter(~ row_number() == 1) %>%
+      dplyr::select(~origin, ~year:hour, ~temp:visib) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(~!is.na(month)) %>%
+      dplyr::mutate(
         time_hour = ~ISOdatetime(year, month, day, hour, 0, 0))
     
     file_path <- paste0(subdir, "/weather.rda")
@@ -144,17 +144,17 @@ make_flights <- function(station, year) {
     planes_lcl <- paste0(subdir, "/planes")
     
     planes_tmp <- tempfile(fileext = ".zip")
-    download.file(planes_src, planes_tmp) 
+    utils::download.file(planes_src, planes_tmp) 
     
     dir.create(planes_lcl)
-    unzip(planes_tmp, exdir = planes_lcl, junkpaths = TRUE)
+    utils::unzip(planes_tmp, exdir = planes_lcl, junkpaths = TRUE)
     
     planes_master <- read.csv(paste0(planes_lcl, "/MASTER.txt"), stringsAsFactors = FALSE, strip.white = TRUE)
     names(planes_master) <- tolower(names(planes_master))
     
     keep <- planes_master %>%
-      tbl_df() %>%
-      select(nnum = n.number, code = aircraftmfrmodelcode, year = year.mfr)
+      dplyr::tbl_df() %>%
+      dplyr::select(nnum = n.number, code = aircraftmfrmodelcode, year = year.mfr)
     
     planes_col_names <- c("mfr", "model", "type.acft", "type.eng", "ac", 
                           "amat", "no.eng", "no.seats", "speed", "na3", "code", "na1", "na2")
@@ -164,12 +164,12 @@ make_flights <- function(station, year) {
                            strip.white = TRUE, header = FALSE, col.names = planes_col_names)
     
     planes_ref <- planes_ref %>%
-      tbl_df() %>%
-      select(code, mfr, model, type.acft, type.eng, no.eng, no.seats, speed)
+      dplyr::tbl_df() %>%
+      dplyr::select(code, mfr, model, type.acft, type.eng, no.eng, no.seats, speed)
     
     planes_all <- keep %>%
-      inner_join(planes_ref, by = "code") %>%
-      select(-code)
+      dplyr::inner_join(planes_ref, by = "code") %>%
+      dplyr::select(-code)
     planes_all$speed[planes_all$speed == 0] <- NA
     planes_all$no.eng[planes_all$no.eng == 0] <- NA
     planes_all$no.seats[planes_all$no.seats == 0] <- NA
@@ -191,12 +191,12 @@ make_flights <- function(station, year) {
     load(flights_ds)
     
     planes <- planes_all %>%
-      select(
+      dplyr::select(
         tailnum, year, type, manufacturer = mfr, model = model,
         engines = no.eng, seats = no.seats, speed, engine
       ) %>%
-      semi_join(flights, "tailnum") %>%
-      arrange(tailnum)
+      dplyr::semi_join(flights, "tailnum") %>%
+      dplyr::arrange(tailnum)
     
     planes_filepath <- paste0(subdir, "/planes.rda")
     save(planes, file = planes_filepath, compress = "xz")
@@ -204,31 +204,31 @@ make_flights <- function(station, year) {
     
   # Download Airports Data -------------------
     
-    if (url.exists("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat")) {
+    if (RCurl::url.exists("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat")) {
       
       airports_subdir <- paste0(subdir, "/airports")
       dir.create(airports_subdir)
       
       airports_data_path <- paste0(airports_subdir, "/airports.dat")
-      download.file(
+      utils::download.file(
         "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
         airports_data_path)
       
-      airports_cols <- cols(
-        id = col_integer(),
-        name = col_character(),
-        city = col_character(),
-        country = col_character(),
-        faa = col_character(),
-        icao = col_character(),
-        lat = col_double(),
-        lon = col_double(),
-        alt = col_integer(),
-        tz = col_double(),
-        dst = col_character(),
-        tzone = col_character(),
-        type = col_character(),
-        source = col_character()
+      airports_cols <- readr::cols(
+        id = readr::col_integer(),
+        name = readr::col_character(),
+        city = readr::col_character(),
+        country = readr::col_character(),
+        faa = readr::col_character(),
+        icao = readr::col_character(),
+        lat = readr::col_double(),
+        lon = readr::col_double(),
+        alt = readr::col_integer(),
+        tz = readr::col_double(),
+        dst = readr::col_character(),
+        tzone = readr::col_character(),
+        type = readr::col_character(),
+        source = readr::col_character()
       )
       
       airports_raw <- read_delim(airports_data_path, ",",
@@ -238,9 +238,9 @@ make_flights <- function(station, year) {
       
       
       airports <- airports_raw %>%
-        filter(country == "United States", faa != "") %>%
-        select(faa, name, lat, lon, alt, tz, dst, tzone) %>%
-        group_by(faa) %>% slice(1) %>% ungroup()
+        dplyr::filter(country == "United States", faa != "") %>%
+        dplyr::select(faa, name, lat, lon, alt, tz, dst, tzone) %>%
+        dplyr::group_by(faa) %>% dplyr::slice(1) %>% dplyr::ungroup()
       
       
       save(airports, file = paste0(subdir, "/airports.rda"), compress = "xz")
@@ -250,20 +250,20 @@ make_flights <- function(station, year) {
     
   # Download Airlines Data -------------------
     
-    if (url.exists("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS")) {
+    if (RCurl::url.exists("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS")) {
       
-      airlines_cols <- cols(
-        Code = col_character(),
-        Description = col_character()
+      airlines_cols <- readr::cols(
+        Code = readr::col_character(),
+        Description = readr::col_character()
       )
       
-      airlines_raw <- read_csv("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS",
+      airlines_raw <- readr::read_csv("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS",
                                col_types = airlines_cols)
       
       airlines <- airlines_raw %>%
-        select(carrier = Code, name = Description) %>%
-        semi_join(flights, by = "carrier") %>%
-        arrange(carrier)
+        dplyr::select(carrier = Code, name = Description) %>%
+        dplyr::semi_join(flights, by = "carrier") %>%
+        dplyr::arrange(carrier)
       
       airlines_filepath <- paste0(subdir, "/airlines.rda")
       save(airlines, file = airlines_filepath, compress = "bzip2")
