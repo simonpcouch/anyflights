0da4cb18edc7247ba63e73972688277efce71105
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-06-16 18:43:33 -0700

    fix weather query

--- R/get_weather.R
+++ R/get_weather.R
@@ -31,20 +31,18 @@ get_weather <- function(station, year, dir) {
   # Download Weather Data --------------------
   
   weather_url <- "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
-  if(!(RCurl::url.exists(weather_url))) stop("Can't access `weather` querying link")
+
   weather_query <- list(
     station = station, data = "all",
     year1 = as.character(year), month1 = "1", day1 = "1",
     year2 = as.character(year), month2 = "12", day2 = "31", tz = "GMT",
     format = "comma", latlon = "no", direct = "yes")
   
-  weather_dir <- paste(dir, "/weather")
-  dir.create(weather_dir, showWarnings = FALSE, recursive = FALSE)
-  
-  r <- httr::GET(weather_url, query = weather_query, write_disk(paste0("./", weather_dir, "/", station, ".csv"), overwrite = TRUE))
-  httr::stop_for_status(r, "Can't access `weather` link for requested location and date range. \n Check data availability at `https://mesonet.agron.iastate.edu`")
-  
-  weather_paths <- dir(weather_dir, full.names = TRUE)
+  if (!dir.exists(dir)) {dir.create(dir, showWarnings = FALSE, recursive = TRUE)}
+  r <- httr::GET(weather_url, query = weather_query, 
+                 write_disk(paste0("./", dir, "/weather.csv"), overwrite = TRUE))
+  httr::stop_for_status(r)                     
+                        
   weather_col_types <- readr::cols(
     .default = readr::col_double(),
     station = readr::col_character(),
@@ -57,40 +55,39 @@ get_weather <- function(station, year, dir) {
     metar = readr::col_character()
   )
   
-  weather_all <- lapply(weather_paths, read_csv, comment = "#", na = "M",
-                        col_names = TRUE, col_types = weather_col_types)
-  weather_raw <- dplyr::bind_rows(weather_all)
-  names(weather_raw) <- c("origin", "valid", "tmpf", "dwpf", "relh", "drct", "sknt",
+  weather_raw <- read_csv(file = paste0("./", dir, "/weather.csv"), comment = "#", na = "M",
+           col_names = TRUE, col_types = weather_col_types)
+  names(weather_raw) <- c("station", "valid", "tmpf", "dwpf", "relh", "drct", "sknt",
                           "p01i", "alti", "mslp", "vsby", "gust",
                           "skyc1", "skyc2", "skyc3", "skyc4",
                           "skyl1", "skyl2", "skyl3", "skyl4", "wxcodes", "metar")
   
   weather <-
     weather_raw %>%
-    dplyr::rename(time = ~valid) %>%
+    dplyr::rename(time = valid) %>%
     dplyr::select(
-      ~origin, ~time, temp = ~tmpf, dewp = ~dwpf, humid = ~relh,
-      wind_dir = ~drct, wind_speed = ~sknt, wind_gust = ~gust,
-      precip = ~p01i, pressure = ~mslp, visib = ~vsby
+      origin = station, time, temp = tmpf, dewp = dwpf, humid = relh,
+      wind_dir = drct, wind_speed = sknt, wind_gust = gust,
+      precip = p01i, pressure = mslp, visib = vsby
     ) %>%
     dplyr::mutate(
-      time = ~as.POSIXct(strptime(time, "%Y-%m-%d %H:%M")),
-      wind_speed = ~as.numeric(wind_speed) * 1.15078, # convert to mpg
-      wind_gust = ~as.numeric(wind_speed) * 1.15078,
+      time = as.POSIXct(strptime(time, "%Y-%m-%d %H:%M")),
+      wind_speed = as.numeric(wind_speed) * 1.15078, # convert to mpg
+      wind_gust = as.numeric(wind_speed) * 1.15078,
       year = year,
-      month = ~lubridate::month(time),
-      day = ~lubridate::mday(time),
-      hour = ~lubridate::hour(time)) %>%
-    dplyr::group_by(~origin, ~month, ~day, ~hour) %>%
-    dplyr::filter(~ row_number() == 1) %>%
-    dplyr::select(~origin, ~year:hour, ~temp:visib) %>%
+      month = lubridate::month(time),
+      day = lubridate::mday(time),
+      hour = lubridate::hour(time)) %>%
+    dplyr::group_by(origin, month, day, hour) %>%
+    dplyr::filter(row_number() == 1) %>%
+    dplyr::select(origin, year:hour, temp:visib) %>%
     dplyr::ungroup() %>%
-    dplyr::filter(~!is.na(month)) %>%
+    dplyr::filter(!is.na(month)) %>%
     dplyr::mutate(
-      time_hour = ~ISOdatetime(year, month, day, hour, 0, 0))
+      time_hour = ISOdatetime(year, month, day, hour, 0, 0))
   
-  file_path <- paste0(dir, "/weather.rda")
-  
-  save(weather, file = file_path, compress = "xz")
+  unlink(paste0("./", dir, "/weather.csv"))
+  weather_file_path <- paste0(dir, "/weather.rda")
+  save(weather, file = weather_file_path, compress = "xz")
   
 }
